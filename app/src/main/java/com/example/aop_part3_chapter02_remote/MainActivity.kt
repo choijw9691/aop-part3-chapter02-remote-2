19adf70a213af3a1aee4cfd0104c6c015fd30491
choijw9691 <choijw9691@gmail.com>
2021-09-03 08:26:59 +0900

    202109030826

--- app/src/main/java/com/example/aop_part3_chapter02_remote/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter02_remote/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.aop_part3_chapter02_remote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewPager : ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+initView()
+
+    }
+
+    private fun initView() {
+        viewPager.adapter=QuotesPagerAdapter(listOf(Quote("나는 생각한다. 고로 나는 존재한다.",
+        "데카르트")))
     }
 }
---
+++ app/src/main/java/com/example/aop_part3_chapter02_remote/Quote.kt
@@ -0,0 +1,7 @@
+package com.example.aop_part3_chapter02_remote
+
+data class Quote(
+    val quote:String,
+    val name: String
+
+)
