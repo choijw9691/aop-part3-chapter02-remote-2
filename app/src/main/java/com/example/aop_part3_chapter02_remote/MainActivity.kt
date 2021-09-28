6f93d80c689f514ebb9adb87911e34b5784829cc
choijw9691 <choijw9691@gmail.com>
2021-09-29 08:24:31 +0900

    202109290824

--- app/src/main/java/com/example/aop_part3_chapter02_remote/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter02_remote/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.aop_part3_chapter02_remote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +18,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 initView()
+initData()
+    }
 
+    private fun initData() {
+val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds=0
+            }
+        )
+        remoteConfig.fetchAndActivate().addOnCompleteListener{
+            if(it.isSuccessful){
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+            }
+
+        }
+
+    }
+
+    private fun parseQuotesJson(json: String):List<Quote> {
+val jsonArray=JSONArray(json)
+        var jsonList= emptyList<JSONObject>()
+        for (index in 0 until jsonArray.length()){
+            val jsonObject=jsonArray.getJSONObject(index)
+        jsonObject?.let{
+            jsonList = jsonList + it
+        }
+        }
+        return jsonList.map {
+            Quote(it.getString("quote"),it.getString("name"))
+        }
     }
 
     private fun initView() {
